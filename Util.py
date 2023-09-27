2e6f62cabd11f941343df675f928d76be8049dea
MHaidarc <mateus.haidar@icloud.com>
2023-09-27 09:57:39 -0300

    clean code

--- Util.py
+++ Util.py
@@ -1,14 +1,18 @@
 import cv2
 
-def PutMark(img,x,y,color=(0,0,255)):
-    PutCircle(img,x,y,15,-1, color)
-    PutCircle(img,x,y,20,2, color)
 
-def PutText(img,text,x,y,color=(0,0,255)):
-    cv2.putText(img, text,(x,y), cv2.FONT_HERSHEY_PLAIN, 5, color, 5)
+def PutMark(img, x, y, color=(0, 0, 255)):
+    PutCircle(img, x, y, 15, -1, color)
+    PutCircle(img, x, y, 20, 2, color)
 
-def PutLine(img,start,end,color=(0,0,255)):
-    cv2.line(img, start, end, color,4)
 
-def PutCircle(img,x,y,radius,fill,color=(0,0,255)):
-    cv2.circle(img, (x,y), radius, color, -1 if isinstance(fill,str) else fill)
+def PutText(img, text, x, y, color=(0, 0, 255)):
+    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 5, color, 5)
+
+
+def PutLine(img, start, end, color=(0, 0, 255)):
+    cv2.line(img, start, end, color, 4)
+
+
+def PutCircle(img, x, y, radius, fill, color=(0, 0, 255)):
+    cv2.circle(img, (x, y), radius, color, -1 if isinstance(fill, str) else fill)
--- pose.py
+++ pose.py
@@ -4,8 +4,11 @@ import math
 
 from Util import PutMark, PutText, PutLine, PutCircle
 
-class PoseDetector():
-    def __init__(self, mode=False, upBody=False, smooth=True, detectionCon=0.5, trackCon=0.5):
+
+class PoseDetector:
+    def __init__(
+        self, mode=False, upBody=False, smooth=True, detectionCon=0.5, trackCon=0.5
+    ):
         self.mode = mode
         self.upBody = upBody
         self.smooth = smooth
@@ -15,7 +18,7 @@ class PoseDetector():
         self.draw = mp.solutions.drawing_utils.draw_landmarks
         self.mpPose = mp.solutions.pose
         self.process = self.mpPose.Pose(self.mode).process
-    
+
     def findPose(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
@@ -25,7 +28,7 @@ class PoseDetector():
             self.draw(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
 
         return img
-        
+
     def findPosition(self, img, draw=True):
         self.lmList = []
 
@@ -33,11 +36,7 @@ class PoseDetector():
             for id, mark in enumerate(self.results.pose_landmarks.landmark):
                 height, width, c = img.shape
 
-                list = [
-                    id,
-                    int(mark.x * width),
-                    int(mark.y * height)
-                ]
+                list = [id, int(mark.x * width), int(mark.y * height)]
 
                 self.lmList.append(list)
 
@@ -45,26 +44,34 @@ class PoseDetector():
                     PutCircle(img, list[1], list[2], 3, -1)
 
         return self.lmList
-    
+
     def findAngle(self, img, p1, p2, p3, draw=True):
         x1, y1 = self.lmList[p1][1:]
         x2, y2 = self.lmList[p2][1:]
         x3, y3 = self.lmList[p3][1:]
 
-        angle = int(math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2)))
+        angle = int(
+            math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
+        )
 
         angle = angle + 360 if angle < 0 else angle
 
         if not draw:
             return
-        
-        PutLine(img,(x1,y1),(x2,y2))
-        PutLine(img,(x3,y3),(x2,y2))
 
-        PutMark(img,x1,y1,(0,255,0))
-        PutMark(img,x2,y2,(0,255,0))
-        PutMark(img,x3,y3,(0,255,0))
+        PutLine(img, (x1, y1), (x2, y2))
+        PutLine(img, (x3, y3), (x2, y2))
 
-        PutText(img, f"{angle} {'grau' if angle < 2 else 'graus'}",x2-50,y2+100, (255,255,0))
+        PutMark(img, x1, y1, (0, 255, 0))
+        PutMark(img, x2, y2, (0, 255, 0))
+        PutMark(img, x3, y3, (0, 255, 0))
+
+        PutText(
+            img,
+            f"{angle} {'grau' if angle < 2 else 'graus'}",
+            x2 - 50,
+            y2 + 100,
+            (255, 255, 0),
+        )
 
         return angle
